3684591c9e1d0dad0a19b8d5c40b7745b1bca4e9
kurimaru2000 <na11012000@gmail.com>
2021-03-26 19:32:51 +0900

    実装完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,24 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    
+  end
+
+  def create
+     @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new      
+    end
   end
 
   private
+  def product_params
+    params.require(:product).permit(:name, :category_id, :status_id, :delivery_fee_id, :perfecture_id, :delivery_days_id, :price, :description, :image).merge(user_id: current_user.id)
+  end                                                                                
 
-  def message_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
-  end
 
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,22 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :perfecture
+  belongs_to :deliveryday
+  belongs_to :deliveryfee
   belongs_to :user
   has_one_attached :image
 
-end
+   with_options presence: true do
+    validates :name
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 } 
+    validates :delivery_fee_id, numericality: { other_than: 1 } 
+    validates :perfecture_id, numericality: { other_than: 1 } 
+    validates :delivery_days_id, numericality: { other_than: 1 } 
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than:299, less_than:10000000 }
+    validates :description
+    validates :image
+  end
+end  
