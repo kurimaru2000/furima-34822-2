4e5e5bb2e74060a2d833cc7e55111f9db9b0758a
kurimaru2000 <na11012000@gmail.com>
2021-03-31 05:55:33 +0900

    実装仮完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless @product.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.update(product_params)
+      redirect_to product_path(@product.id), method: :get
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
