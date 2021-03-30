0373b0e76d74cbfd9e5a9b3c5a29679c4eacb63a
kurimaru2000 <na11012000@gmail.com>
2021-03-31 07:15:04 +0900

    商品情報編集機能実装完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @products = Product.includes(:user).order('created_at DESC')
   end
