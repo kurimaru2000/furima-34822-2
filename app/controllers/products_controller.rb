16a6c5c80ad10e600763f38526fd1ec8e6373c5f
kurimaru2000 <na11012000@gmail.com>
2021-03-31 11:33:52 +0900

    商品情報編集機能訂正 -2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :product_find, only:[:show, :edit, :update]
   def index
     @products = Product.includes(:user).order('created_at DESC')
   end
@@ -18,28 +19,22 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless @product.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @product = Product.find(params[:id])
-      unless @product.user_id == current_user.id
-        redirect_to action: :index
+    @product.update(product_params)
+    redirect_to action: :indec unless @product.user_id == current_user.id
+      if @product.update(product_params)
+        redirect_to product_path(@product.id), method: :get
       else
-        @product.update(product_params)
-          if @product.update(product_params)
-               redirect_to product_path(@product.id), method: :get
-          else
-            render :edit
-          end
-      end
+        render :edit
+      end 
   end
 
   # def destroy
@@ -53,9 +48,13 @@ class ProductsController < ApplicationController
   # end
 
   private
-
   def product_params
     params.require(:product).permit(:name, :category_id, :status_id, :delivery_fee_id, :perfecture_id, :delivery_days_id,
                                     :price, :description, :image).merge(user_id: current_user.id)
   end
+
+  def product_find
+    @product = Product.find(params[:id])
+  end
+
 end
